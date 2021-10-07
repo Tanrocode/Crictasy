d05d3d0dc7519e89bc87c38e8588ed61d1eb3813
Vishal <vishalbhat2006@gmail.com>
2021-10-06 20:41:48 -0700

    Basic bot setup

---
+++ src/discord/app.ts
@@ -0,0 +1,16 @@
+import DiscordJS, { Intents } from 'discord.js'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const client = new DiscordJS.Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+})
+
+client.on('ready', () => {
+    console.log("Bot iz ready")
+})
+
+client.login(process.env.TOKEN)
