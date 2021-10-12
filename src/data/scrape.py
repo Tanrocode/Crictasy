fd163e3164125f15d4904d04e94346a8670f308c
Tanrocode <herotanush@gmail.com>
2021-10-11 19:44:35 -0700

    bug fix #1

--- src/data/scrape.py
+++ src/data/scrape.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 PATH = "C:\Program Files (x86)\geckodriver.exe"
 
@@ -10,12 +14,15 @@ print(driver.title)
 
 def get_run_data(date):
     try:
-        choose_data = Select(driver.find_element_by_class_name("drop-down__clickzone.js-dropdown-trigger"))
-        choose_data.select_by_visible_text(str(date))
+        wait = WebDriverWait(driver, 15)
+        wait.until(EC.visibility_of_element_located((By.XPATH, "/html/body/main/div[2]/div/div/div[2]/div[1]/div[1]")))
+        driver.find_element_by_xpath("/html/body/main/div[2]/div/div/div[2]/div[1]/div[1]").click()
+        choose_data = driver.find_element_by_class_name("drop-down__dropdown-list__option")
+        choose_data.find_element_by_xpath(f"/html/body/main/div[2]/div/div/div[2]/div[1]/ul/li[{(2021-date)+1}]").click()
         elem = driver.find_elements_by_class_name("top-players__r  is-active")
-        print(elem.text)
+        print(elem)
     finally:
         driver.close()
 
-print(get_run_data(2021))
+print(get_run_data(2020))
 
